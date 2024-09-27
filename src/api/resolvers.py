dff5936d11a2987b9a4b10bbf19573365132c36e
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-09-27 13:55:28 +0200

    feat: tweak sentry exception config

--- src/api/resolvers.py
+++ src/api/resolvers.py
@@ -19,30 +19,29 @@ class ChunkStrategy(Enum):
     RECURSIVE = "recursive"
 
 
+class RagException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
 @strawberry.type
 class Query:
     @strawberry.field
     async def get_response(self, query: str, info: Info) -> RAGResult:
         try:
-            if not info.context["is_authenticated"]:
-                raise Exception("User is not authenticated")
-
             user_id = info.context['user']['id']
-
             result = await retrieve_documents(query, user_id)
             return RAGResult(result=result)
         except Exception as e:
-            raise Exception(str(e))
+            raise RagException(str(e))
 
     @strawberry.field
     async def get_me(self, info: Info) -> RAGResult:
         try:
-            if not info.context["is_authenticated"]:
-                raise Exception("User is not authenticated")
             user = info.context["user"]
             return RAGResult(result=f"{user}")
         except Exception as e:
-            raise Exception(str(e))
+            raise RagException(str(e))
 
 
 @strawberry.type
@@ -50,40 +49,31 @@ class Mutation:
     @strawberry.mutation
     async def update_store(self, chunk_strategy: ChunkStrategy, info: Info) -> RAGResult:
         try:
-            if not info.context["is_authenticated"]:
-                raise Exception("User is not authenticated")
-
             user_id = info.context['user']['id']
 
             DOCUMENTS_DIR = get_env_var("DOCUMENTS_DIR")
             documents = load_docs_from_directory(DOCUMENTS_DIR)
             chunked_documents = []
             if chunk_strategy == ChunkStrategy.SEMANTIC:
-                print("Semantic chunking start")
                 chunked_documents = semantic_chunk_documents(documents)
-                print("Semantic chunking end")
             elif chunk_strategy == ChunkStrategy.RECURSIVE:
-                print("Recusrive chunking start")
                 chunked_documents = recursive_chunk_documents(documents)
-                print("Recusrive chunking end")
             else:
                 raise ValueError("Unknown chunk strategy")
 
             update_vector_store(chunked_documents, user_id)
             return RAGResult(result="Store updated")
         except ValueError as e:
-            raise Exception(str(e))
+            raise Exception(e)
         except Exception as e:
-            raise Exception(str(e))
+            raise RagException(e)
 
     @strawberry.mutation
     async def delete_store(self, info: Info) -> RAGResult:
         try:
-            if not info.context["is_authenticated"]:
-                raise Exception("User is not authenticated")
             user_id = info.context['user']['id']
 
             delete_vector_store(user_id=user_id)
             return RAGResult(result="Store deleted")
         except Exception as e:
-            raise Exception(str(e))
+            raise RagException(str(e))
--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,18 +1,46 @@
 import sentry_sdk
+from fastapi import HTTPException
+
 from src.utils.get_env import get_env_var
+from src.api.resolvers import RagException
 
 
 def init_sentry():
-
     try:
         SENTRY_DSN = get_env_var("SENTRY_DSN")
         ENVIRONMENT = get_env_var("ENVIRONMENT")
 
-        if (ENVIRONMENT == "production"):
+        if (ENVIRONMENT != "local"):
             sentry_sdk.init(
                 dsn=''.join(SENTRY_DSN),
                 traces_sample_rate=1.0,
+                before_send=before_send,
+                debug=False,
+                environment=ENVIRONMENT,
             )
+
+            sentry_sdk.set_level("error")
 
     except Exception as e:
         print(f"Error initializing Sentry: {e}")
+
+
+def before_send(event, hint):
+    if should_send_event(event, hint):
+        return event
+    return None
+
+
+def should_send_event(event, hint):
+    exception = hint.get('exc_info')
+    if exception:
+        exc_type, exc_value, tb = exception
+        # Always Catch RagException
+        if isinstance(exc_value, RagException):
+            return True
+
+        # Ignored Exceptions
+        if isinstance(exc_value, ValueError) or isinstance(exc_value, HTTPException):
+            return False
+
+    return True
