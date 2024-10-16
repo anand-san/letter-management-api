776b62120a158289cf0cabb55b4dc155250253df
Anand Sandilya <17026547+anand-san@users.noreply.github.com>
2024-10-17 00:45:06 +0200

    fix dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,20 @@
-FROM denoland/deno:alpine
+FROM denoland/deno:2.0.0
 
 WORKDIR /app
 
-COPY . .
+# Copy only necessary files
+COPY deno.json .
+COPY deno.lock .
+COPY deps.json .
+COPY main.ts .
+COPY env.ts .
+
+COPY src src
+COPY templates templates
+
+# Cache dependencies
+RUN deno cache deps.json
 
 RUN deno cache main.ts
 
-CMD ["deno", "run", "--allow-net", "--allow-env", "--allow-read", "--allow-run", "main.ts"]
+CMD ["deno", "run", "--allow-net", "--allow-env", "--allow-read", "--allow-run", "--env", "main.ts"]
--- env.ts
+++ env.ts
@@ -1,5 +1,4 @@
-import { loadSync } from "std/dotenv/mod.ts";
-
+import "jsr:@std/dotenv/load";
 export interface Env {
   [key: string]: string;
 
@@ -13,6 +12,22 @@ export interface Env {
   OPENAI_MODEL: string;
 }
 
-const env = loadSync() as Env;
+function getEnv(): Env {
+  // Use Deno.env to get environment variables
+  return {
+    PORT: Deno.env.get("PORT") || "",
+    FIREBASE_SERVICE_ACCOUNT: Deno.env.get("FIREBASE_SERVICE_ACCOUNT") || "",
+    FIREBASE_STORAGE_BUCKET: Deno.env.get("FIREBASE_STORAGE_BUCKET") || "",
+    GOOGLE_CLOUD_PROJECT_ID: Deno.env.get("GOOGLE_CLOUD_PROJECT_ID") || "",
+    GOOGLE_CLOUD_DOCUMENT_AI_PROCESSOR_REGION:
+      Deno.env.get("GOOGLE_CLOUD_DOCUMENT_AI_PROCESSOR_REGION") || "",
+    GOOGLE_CLOUD_DOCUMENT_AI_PROCESSOR_ID:
+      Deno.env.get("GOOGLE_CLOUD_DOCUMENT_AI_PROCESSOR_ID") || "",
+    OPENAI_API_KEY: Deno.env.get("OPENAI_API_KEY") || "",
+    OPENAI_MODEL: Deno.env.get("OPENAI_MODEL") || "",
+  };
+}
+
+const env: Env = getEnv();
 
 export default env;
